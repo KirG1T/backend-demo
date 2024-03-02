c8bb8ca8f39b1a809669331f76d1fe56ea2b1914
Kirill Ivashkov <kirillivashk@gmail.com>
2024-03-02 14:30:16 +0200

    add endpoint

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello Vladimir, Когда проект доделаешь? )))');
 });
 
+app.get('/about', (req: Request, res: Response) => {
+  res.send('testing about page');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
