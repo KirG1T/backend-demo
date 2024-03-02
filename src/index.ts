5d4f014b38891ad3a1248e2198ce639c7bcf756f
Kirill Ivashkov <kirillivashk@gmail.com>
2024-03-02 14:18:21 +0200

    text fix

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from Dnipro!');
+  res.send('Hello Vladimir, Когда проект доделаешь? )))');
 });
 
 app.listen(port, () => {
